bd9b92581c637e8b99f6868a2b7fc2469acf4f12
Itai Tivoni <itai@ddaylabs.com>
2024-01-17 22:33:53 +0200

    added a new test

--- automation/e2e/test_back_end.py
+++ automation/e2e/test_back_end.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class TestBackEnd(object):
 
     def test_numbers(self):
@@ -5,3 +8,8 @@ class TestBackEnd(object):
         a = 1
         b = 3
         assert a + b == 4
+
+    def test_status_code(self, url):
+
+        res = requests.get(url)
+        assert res.status_code == 200
